e3452f5f34737b51ac56a25649eef73f2336f46b
Kiooeht <kiooeht@gmail.com>
2020-03-26 16:41:30 -0700

    Create Burning Stabs as custom power
    Avoids some patches and doesn't change Painful Stabs for other monsters

--- src/main/kotlin/patches/city/BookOfStabbingBurns.kt
+++ src/main/kotlin/patches/city/BookOfStabbingBurns.kt
@@ -1,15 +1,15 @@
 package com.evacipated.cardcrawl.mod.humilty.patches.city
 
+import com.evacipated.cardcrawl.mod.humilty.powers.BurningStabsPower
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
-import com.megacrit.cardcrawl.cards.status.Burn
-import com.megacrit.cardcrawl.cards.status.Wound
+import com.megacrit.cardcrawl.monsters.city.BookOfStabbing
 import com.megacrit.cardcrawl.powers.PainfulStabsPower
 import javassist.expr.ExprEditor
 import javassist.expr.NewExpr
 
 @SpirePatch(
-    clz = PainfulStabsPower::class,
-    method = "onInflictDamage"
+    clz = BookOfStabbing::class,
+    method = "usePreBattleAction"
 )
 class BookOfStabbingBurns {
     companion object {
@@ -17,8 +17,8 @@ class BookOfStabbingBurns {
         fun Instrument(): ExprEditor =
             object : ExprEditor() {
                 override fun edit(e: NewExpr) {
-                    if (e.className == Wound::class.qualifiedName) {
-                        e.replace("\$_ = new ${Burn::class.qualifiedName}();")
+                    if (e.className == PainfulStabsPower::class.qualifiedName) {
+                        e.replace("\$_ = new ${BurningStabsPower::class.qualifiedName}(\$\$);")
                     }
                 }
             }
