1ba2074a82f1a975e427d270bfea1c35b5ca7bfa
Kiooeht <kiooeht@gmail.com>
2020-03-28 19:31:04 -0700

    Last move AI helpers

---
+++ src/main/kotlin/patches/utils/LastMoveExtensions.kt
@@ -0,0 +1,19 @@
+package com.evacipated.cardcrawl.mod.humilty.patches.utils
+
+import com.megacrit.cardcrawl.monsters.AbstractMonster
+
+fun AbstractMonster.isFirstMove(): Boolean {
+    return moveHistory.isEmpty()
+}
+
+fun AbstractMonster.lastMove(move: Byte): Boolean {
+    return moveHistory.isNotEmpty() && moveHistory.last() == move
+}
+
+fun AbstractMonster.lastMoveBefore(move: Byte): Boolean {
+    return moveHistory.size >= 2 && moveHistory[moveHistory.size - 2] == move
+}
+
+fun AbstractMonster.lastTwoMoves(move: Byte): Boolean {
+    return moveHistory.size >= 2 && moveHistory.last() == move && moveHistory[moveHistory.size - 2] == move
+}
