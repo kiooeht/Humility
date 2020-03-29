dca4a15a75757ea7a000be0104fe87adfb30d2cd
Kiooeht <kiooeht@gmail.com>
2020-03-28 18:37:32 -0700

    Fungi Beast buff
    Spore Clouds buff will automatically go off after 2-4 turns as well as on death

---
+++ src/main/kotlin/patches/exordium/FungiBeastExplodingSpores.kt
@@ -0,0 +1,27 @@
+package com.evacipated.cardcrawl.mod.humilty.patches.exordium
+
+import com.evacipated.cardcrawl.mod.humilty.powers.ExplodingSporeCloudPower
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import com.megacrit.cardcrawl.monsters.exordium.FungiBeast
+import com.megacrit.cardcrawl.powers.SporeCloudPower
+import javassist.expr.ExprEditor
+import javassist.expr.NewExpr
+
+@SpirePatch(
+    clz = FungiBeast::class,
+    method = "usePreBattleAction"
+)
+class FungiBeastExplodingSpores {
+    companion object {
+        @JvmStatic
+        fun Instrument(): ExprEditor =
+            object : ExprEditor() {
+                override fun edit(e: NewExpr) {
+                    if (e.className == SporeCloudPower::class.qualifiedName) {
+                        e.replace("\$_ = new ${ExplodingSporeCloudPower::class.qualifiedName}($$, ${AbstractDungeon::class.qualifiedName}.monsterHpRng.random(2, 4));")
+                    }
+                }
+            }
+    }
+}
