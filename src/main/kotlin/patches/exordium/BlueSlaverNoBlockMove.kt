e78372c698986fe104dd5e4c3ecb5c69c628afb5
Kiooeht <kiooeht@gmail.com>
2020-03-29 17:36:19 -0700

    Give Blue Slaver new move
    Trip: Gives player the No Block debuff for 1 turn
    25% chance to use, can only use once

---
+++ src/main/kotlin/patches/exordium/BlueSlaverNoBlockMove.kt
@@ -0,0 +1,80 @@
+package com.evacipated.cardcrawl.mod.humilty.patches.exordium
+
+import com.evacipated.cardcrawl.mod.humilty.HumilityMod
+import com.evacipated.cardcrawl.modthespire.lib.SpireField
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn
+import com.megacrit.cardcrawl.actions.animations.VFXAction
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.core.Settings
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import com.megacrit.cardcrawl.monsters.AbstractMonster
+import com.megacrit.cardcrawl.monsters.exordium.SlaverBlue
+import com.megacrit.cardcrawl.powers.NoBlockPower
+import com.megacrit.cardcrawl.vfx.combat.EntangleEffect
+
+class BlueSlaverNoBlockMove {
+    companion object {
+        const val TRIP: Byte = 8
+    }
+
+    @SpirePatch(
+        clz = SlaverBlue::class,
+        method = SpirePatch.CLASS
+    )
+    class Fields {
+        companion object {
+            @JvmField
+            val usedEntangle: SpireField<Boolean> = SpireField { false }
+        }
+    }
+
+    @SpirePatch(
+        clz = SlaverBlue::class,
+        method = "getMove"
+    )
+    class GetMove {
+        companion object {
+            private var TRIP_NAME: String? = null
+
+            @JvmStatic
+            fun Prefix(__instance: SlaverBlue, num: Int): SpireReturn<Unit?> {
+                if (num >= 75 && !Fields.usedEntangle.get(__instance)) {
+                    if (TRIP_NAME == null) {
+                        val strings = CardCrawlGame.languagePack.getMonsterStrings(HumilityMod.makeID(SlaverBlue.ID))
+                        TRIP_NAME = strings?.MOVES?.get(0)
+                    }
+                    __instance.setMove(TRIP_NAME ?: "[MISSING]", TRIP, AbstractMonster.Intent.STRONG_DEBUFF)
+                    return SpireReturn.Return(null)
+                }
+                return SpireReturn.Continue()
+            }
+        }
+    }
+
+    @SpirePatch(
+        clz = SlaverBlue::class,
+        method = "takeTurn"
+    )
+    class TakeTurn {
+        companion object {
+            @JvmStatic
+            fun Prefix(__instance: SlaverBlue) {
+                if (__instance.nextMove == TRIP) {
+                    if (!Settings.FAST_MODE) {
+                        AbstractDungeon.actionManager.addToBottom(VFXAction(EntangleEffect(
+                            __instance.hb.cX - 70f * Settings.scale,
+                            __instance.hb.cY + 10f * Settings.scale,
+                            AbstractDungeon.player.hb.cX,
+                            AbstractDungeon.player.hb.cY
+                            ), 0.5f
+                        ))
+                    }
+                    AbstractDungeon.actionManager.addToBottom(ApplyPowerAction(AbstractDungeon.player, __instance, NoBlockPower(AbstractDungeon.player, 1, true), 1))
+                    Fields.usedEntangle.set(__instance, true)
+                }
+            }
+        }
+    }
+}
