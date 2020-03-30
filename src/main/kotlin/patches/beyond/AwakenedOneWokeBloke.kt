cae5ebee471294672bce7e5e087e71abb03a5d98
Kiooeht <kiooeht@gmail.com>
2020-03-29 18:33:14 -0700

    Awakened One gets another phase
    Gets a second rebirth
    Afterwards, gains 10 Ritual but Max HP is lowered to 150 (170 on Asc 9+)

---
+++ src/main/kotlin/patches/beyond/AwakenedOneWokeBloke.kt
@@ -0,0 +1,155 @@
+package com.evacipated.cardcrawl.mod.humilty.patches.beyond
+
+import com.evacipated.cardcrawl.modthespire.lib.*
+import com.megacrit.cardcrawl.actions.animations.VFXAction
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction
+import com.megacrit.cardcrawl.actions.common.ChangeStateAction
+import com.megacrit.cardcrawl.actions.common.HealAction
+import com.megacrit.cardcrawl.actions.common.RollMoveAction
+import com.megacrit.cardcrawl.actions.unique.CanLoseAction
+import com.megacrit.cardcrawl.actions.unique.CannotLoseAction
+import com.megacrit.cardcrawl.blights.Shield
+import com.megacrit.cardcrawl.cards.DamageInfo
+import com.megacrit.cardcrawl.daily.mods.Colossus
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import com.megacrit.cardcrawl.helpers.ModHelper
+import com.megacrit.cardcrawl.monsters.beyond.AwakenedOne
+import com.megacrit.cardcrawl.powers.RitualPower
+import com.megacrit.cardcrawl.vfx.AwakenedWingParticle
+import com.megacrit.cardcrawl.vfx.combat.IntenseZoomEffect
+import javassist.CtBehavior
+
+class AwakenedOneWokeBloke {
+    companion object {
+        private const val SECOND_REBIRTH = "SECOND_REBIRTH"
+        private const val RITUAL = 10
+    }
+
+    @SpirePatch(
+        clz = AwakenedOne::class,
+        method = SpirePatch.CLASS
+    )
+    class Fields {
+        companion object {
+            @JvmField
+            val trueForm: SpireField<Boolean> = SpireField { false }
+        }
+    }
+
+    @SpirePatch(
+        clz = AwakenedOne::class,
+        method = "changeState"
+    )
+    class AddRitual {
+        companion object {
+            @JvmStatic
+            fun Postfix(__instance: AwakenedOne, key: String) {
+                if (key == "REBIRTH") {
+                    // Undo the CanLoseAction on rebirth
+                    AbstractDungeon.actionManager.addToBottom(CannotLoseAction())
+                } else if (key == SECOND_REBIRTH) {
+                    __instance.maxHealth = if (AbstractDungeon.ascensionLevel >= 9) {
+                        170
+                    } else {
+                        150
+                    }
+                    AbstractDungeon.player.getBlight(Shield.ID)?.let {
+                        __instance.maxHealth = (__instance.maxHealth * it.effectFloat()).toInt()
+                    }
+
+                    if (ModHelper.isModEnabled(Colossus.ID)) {
+                        __instance.currentHealth = (__instance.currentHealth * 1.5f).toInt()
+                    }
+
+                    //__instance.state.setAnimation(0, "Idle_2", true)
+                    __instance.halfDead = false
+
+                    AbstractDungeon.actionManager.addToBottom(HealAction(__instance, __instance, __instance.maxHealth))
+                    AbstractDungeon.actionManager.addToBottom(ApplyPowerAction(__instance, __instance, RitualPower(__instance, RITUAL, false), RITUAL))
+                    AbstractDungeon.actionManager.addToBottom(CanLoseAction())
+                }
+            }
+        }
+    }
+
+    @SpirePatch(
+        clz = AwakenedOne::class,
+        method = "takeTurn"
+    )
+    class InitiateSecondRebirth {
+        companion object {
+            @JvmStatic
+            fun Prefix(__instance: AwakenedOne, ___REBIRTH: Byte): SpireReturn<Unit?> {
+                if (__instance.nextMove == ___REBIRTH && Fields.trueForm.get(__instance)) {
+                    AbstractDungeon.actionManager.addToBottom(
+                        VFXAction(
+                            __instance,
+                            IntenseZoomEffect(
+                                __instance.hb.cX,
+                                __instance.hb.cY,
+                                true
+                            ),
+                            0.05f,
+                            true
+                        )
+                    )
+                    AbstractDungeon.actionManager.addToBottom(ChangeStateAction(__instance, SECOND_REBIRTH))
+                    AbstractDungeon.actionManager.addToBottom(RollMoveAction(__instance))
+                    return SpireReturn.Return(null)
+                }
+                return SpireReturn.Continue()
+            }
+        }
+    }
+
+    @SpirePatch(
+        clz = AwakenedOne::class,
+        method = "damage"
+    )
+    class SetTrueForm {
+        companion object {
+            @JvmStatic
+            @SpireInsertPatch(
+                locator = Locator::class
+            )
+            fun Insert(__instance: AwakenedOne, info: DamageInfo, ___form1: Boolean) {
+                if (!___form1) {
+                    Fields.trueForm.set(__instance, true)
+                }
+            }
+        }
+
+        private class Locator : SpireInsertLocator() {
+            override fun Locate(ctBehavior: CtBehavior?): IntArray {
+                val finalMatcher = Matcher.FieldAccessMatcher(AwakenedOne::class.java, "form1")
+                return LineFinder.findInOrder(ctBehavior, arrayListOf<Matcher>(finalMatcher), finalMatcher)
+            }
+        }
+    }
+
+    @SpirePatch(
+        clz = AwakenedOne::class,
+        method = "update"
+    )
+    class MoreParticles {
+        companion object {
+            @JvmStatic
+            @SpireInsertPatch(
+                locator = Locator::class
+            )
+            fun Insert(__instance: AwakenedOne, ___wParticles: ArrayList<AwakenedWingParticle>) {
+                if (!__instance.halfDead && Fields.trueForm.get(__instance)) {
+                    ___wParticles.add(AwakenedWingParticle())
+                    ___wParticles.add(AwakenedWingParticle())
+                }
+            }
+        }
+
+        private class Locator : SpireInsertLocator() {
+            override fun Locate(ctBehavior: CtBehavior?): IntArray {
+                val finalMatcher = Matcher.NewExprMatcher(AwakenedWingParticle::class.java)
+                return LineFinder.findInOrder(ctBehavior, finalMatcher)
+            }
+        }
+    }
+}
