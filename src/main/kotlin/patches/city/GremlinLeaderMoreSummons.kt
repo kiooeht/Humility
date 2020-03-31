ae9ef391c9f630266d3a11bebb95648ad4e2de20
Kiooeht <kiooeht@gmail.com>
2020-03-30 20:24:19 -0700

    Make Gremlin Leader spawn 1 additional gremlin with Rally

--- src/main/kotlin/patches/city/GremlinLeaderMoreSummons.kt
+++ src/main/kotlin/patches/city/GremlinLeaderMoreSummons.kt
@@ -1,13 +1,14 @@
 package com.evacipated.cardcrawl.mod.humilty.patches.city
 
 import basemod.ReflectionHacks
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
+import com.evacipated.cardcrawl.modthespire.lib.*
+import com.megacrit.cardcrawl.actions.unique.SummonGremlinAction
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.helpers.MonsterHelper
 import com.megacrit.cardcrawl.monsters.AbstractMonster
 import com.megacrit.cardcrawl.monsters.MonsterGroup
 import com.megacrit.cardcrawl.monsters.city.GremlinLeader
+import javassist.CtBehavior
 
 class GremlinLeaderMoreSummons {
     @SpirePatch(
@@ -41,7 +42,7 @@ class GremlinLeaderMoreSummons {
                     "POSY",
                     GremlinLeader.POSY.copyOf(GremlinLeader.POSY.size + 1)
                 )
-                GremlinLeader.POSX[3] = -590f
+                GremlinLeader.POSX[3] = -690f
                 GremlinLeader.POSY[3] = 2f
             }
         }
@@ -77,6 +78,56 @@ class GremlinLeaderMoreSummons {
             fun Insert(__instance: GremlinLeader) {
                 __instance.gremlins[2] = AbstractDungeon.getMonsters().monsters[2]
                 __instance.gremlins[3] = null
+            }
+        }
+    }
+
+    @SpirePatch(
+        clz = GremlinLeader::class,
+        method = "takeTurn"
+    )
+    class RallyMore {
+        companion object {
+            @JvmStatic
+            @SpireInsertPatch(
+                locator = Locator::class
+            )
+            fun Insert(__instance: GremlinLeader) {
+                AbstractDungeon.actionManager.addToBottom(SummonGremlinAction(__instance.gremlins))
+            }
+        }
+
+        private class Locator : SpireInsertLocator() {
+            override fun Locate(ctBehavior: CtBehavior?): IntArray {
+                val finalMatcher = Matcher.NewExprMatcher(SummonGremlinAction::class.java)
+                return LineFinder.findInOrder(ctBehavior, finalMatcher)
+            }
+        }
+    }
+
+    @SpirePatch(
+        clz = SummonGremlinAction::class,
+        method = "getRandomGremlin"
+    )
+    class UseNewPosition {
+        companion object {
+            @JvmStatic
+            @SpireInsertPatch(
+                locator = Locator::class,
+                localvars = ["x", "y"]
+            )
+            fun aasdf(__instance: SummonGremlinAction, slot: Int, @ByRef(type = "float") x: Array<Float>, @ByRef(type = "float") y: Array<Float>) {
+                if (slot == 3) {
+                    x[0] = GremlinLeader.POSX[3]
+                    y[0] = GremlinLeader.POSY[3]
+                }
+            }
+        }
+
+        private class Locator : SpireInsertLocator() {
+            override fun Locate(ctBehavior: CtBehavior?): IntArray {
+                val finalMatcher = Matcher.MethodCallMatcher(MonsterHelper::class.java, "getGremlin")
+                return LineFinder.findInOrder(ctBehavior, finalMatcher)
             }
         }
     }
