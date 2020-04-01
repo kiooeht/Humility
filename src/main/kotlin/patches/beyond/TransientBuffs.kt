46f94eccd0a3a0c4eeadb5f8c3245825178e66c0
Kiooeht <kiooeht@gmail.com>
2020-04-01 16:42:58 -0700

    Ensure Transient can't crash by running out of moves

--- src/main/kotlin/patches/beyond/TransientBuffs.kt
+++ src/main/kotlin/patches/beyond/TransientBuffs.kt
@@ -2,6 +2,8 @@ package com.evacipated.cardcrawl.mod.humilty.patches.beyond
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction
+import com.megacrit.cardcrawl.actions.common.DamageAction
+import com.megacrit.cardcrawl.cards.DamageInfo
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.monsters.beyond.Transient
 import com.megacrit.cardcrawl.powers.FadingPower
@@ -9,25 +11,52 @@ import com.megacrit.cardcrawl.powers.RegenerateMonsterPower
 import javassist.expr.ExprEditor
 import javassist.expr.NewExpr
 
-@SpirePatch(
-    clz = Transient::class,
-    method = "usePreBattleAction"
-)
 class TransientBuffs {
-    companion object {
-        @JvmStatic
-        fun Instrument(): ExprEditor =
-            object : ExprEditor() {
-                override fun edit(e: NewExpr) {
-                    if (e.className == FadingPower::class.qualifiedName) {
-                        e.replace("\$_ = \$proceed(\$1, \$2 + 1);")
+    @SpirePatch(
+        clz = Transient::class,
+        method = "usePreBattleAction"
+    )
+    class AddBuffs {
+        companion object {
+            @JvmStatic
+            fun Instrument(): ExprEditor =
+                object : ExprEditor() {
+                    override fun edit(e: NewExpr) {
+                        if (e.className == FadingPower::class.qualifiedName) {
+                            e.replace("\$_ = \$proceed(\$1, \$2 + 1);")
+                        }
                     }
                 }
-            }
 
-        @JvmStatic
-        fun Postfix(__instance: Transient) {
-            AbstractDungeon.actionManager.addToBottom(ApplyPowerAction(__instance, __instance, RegenerateMonsterPower(__instance, 999), 999))
+            @JvmStatic
+            fun Postfix(__instance: Transient) {
+                AbstractDungeon.actionManager.addToBottom(
+                    ApplyPowerAction(
+                        __instance,
+                        __instance,
+                        RegenerateMonsterPower(__instance, 999),
+                        999
+                    )
+                )
+            }
+        }
+    }
+
+    @SpirePatch(
+        clz = Transient::class,
+        method = "takeTurn"
+    )
+    class FixExtraFadingAttacks {
+        companion object {
+            @JvmStatic
+            fun Instrument(): ExprEditor =
+                object : ExprEditor() {
+                    override fun edit(e: NewExpr) {
+                        if (e.className == DamageAction::class.qualifiedName) {
+                            e.replace("\$_ = \$proceed(\$1, new ${DamageInfo::class.qualifiedName}(this, startingDeathDmg + count * 10), \$3);")
+                        }
+                    }
+                }
         }
     }
 }
