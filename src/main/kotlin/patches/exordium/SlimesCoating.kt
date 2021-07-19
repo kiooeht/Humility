9e12c308756e7e20d9a7cfaa0da7b9cd19f81123
Kiooeht <kiooeht@gmail.com>
2021-07-18 22:31:14 -0700

    Fix Medium/Small Slimes spawned later via split not having Slime Coating

--- src/main/kotlin/patches/exordium/SlimesCoating.kt
+++ src/main/kotlin/patches/exordium/SlimesCoating.kt
@@ -2,9 +2,9 @@ package com.evacipated.cardcrawl.mod.humilty.patches.exordium
 
 import com.evacipated.cardcrawl.mod.humilty.patches.utils.addPreBattleAction
 import com.evacipated.cardcrawl.mod.humilty.powers.SlimeCoatingPower
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatches
+import com.evacipated.cardcrawl.modthespire.lib.*
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction
+import com.megacrit.cardcrawl.actions.common.SpawnMonsterAction
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.monsters.AbstractMonster
 import com.megacrit.cardcrawl.monsters.exordium.AcidSlime_M
@@ -13,34 +13,61 @@ import com.megacrit.cardcrawl.monsters.exordium.SpikeSlime_M
 import com.megacrit.cardcrawl.monsters.exordium.SpikeSlime_S
 import javassist.CtBehavior
 
-@SpirePatches(
-    SpirePatch(
-        clz = AcidSlime_S::class,
-        method = "takeTurn"
-    ),
-    SpirePatch(
-        clz = AcidSlime_M::class,
-        method = "takeTurn"
-    ),
-    SpirePatch(
-        clz = SpikeSlime_S::class,
-        method = "takeTurn"
-    ),
-    SpirePatch(
-        clz = SpikeSlime_M::class,
-        method = "takeTurn"
-    )
-)
 class SlimesCoating {
-    companion object {
-        @JvmStatic
-        fun Raw (ctBehavior: CtBehavior) {
-            ctBehavior.addPreBattleAction(::doPreBattleAction)
+    @SpirePatches(
+        SpirePatch(
+            clz = AcidSlime_S::class,
+            method = "takeTurn"
+        ),
+        SpirePatch(
+            clz = AcidSlime_M::class,
+            method = "takeTurn"
+        ),
+        SpirePatch(
+            clz = SpikeSlime_S::class,
+            method = "takeTurn"
+        ),
+        SpirePatch(
+            clz = SpikeSlime_M::class,
+            method = "takeTurn"
+        )
+    )
+    class PreBattle {
+        companion object {
+            @JvmStatic
+            fun Raw(ctBehavior: CtBehavior) {
+                ctBehavior.addPreBattleAction(::doPreBattleAction)
+            }
+
+            @JvmStatic
+            fun doPreBattleAction(__instance: AbstractMonster) {
+                AbstractDungeon.actionManager.addToBottom(ApplyPowerAction(__instance, __instance, SlimeCoatingPower(__instance, 1), 1))
+            }
+        }
+    }
+
+    @SpirePatch2(
+        clz = SpawnMonsterAction::class,
+        method = "update"
+    )
+    class AddSlimeCoatingToSplit {
+        companion object {
+            @JvmStatic
+            @SpireInsertPatch(
+                locator = Locator::class
+            )
+            fun Insert(___m: AbstractMonster) {
+                if (___m is AcidSlime_S || ___m is AcidSlime_M || ___m is SpikeSlime_S || ___m is SpikeSlime_M) {
+                    PreBattle.doPreBattleAction(___m)
+                }
+            }
         }
 
-        @JvmStatic
-        fun doPreBattleAction(__instance: AbstractMonster) {
-            AbstractDungeon.actionManager.addToBottom(ApplyPowerAction(__instance, __instance, SlimeCoatingPower(__instance, 1), 1))
+        private class Locator : SpireInsertLocator() {
+            override fun Locate(ctBehavior: CtBehavior?): IntArray {
+                val finalMatcher = Matcher.FieldAccessMatcher(SpawnMonsterAction::class.java, "minion")
+                return LineFinder.findInOrder(ctBehavior, finalMatcher)
+            }
         }
     }
 }
