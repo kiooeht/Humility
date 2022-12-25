e1452900872d02739a3b457949818602676c5870
Kiooeht <kiooeht@gmail.com>
2022-12-24 18:20:41 -0800

    Revert "Time Eater buff changed"
    This reverts commit 2f00e7be92a4bf9f495cc123e174578f10eba92a.

--- src/main/kotlin/HumilityMod.kt
+++ src/main/kotlin/HumilityMod.kt
@@ -1,16 +1,12 @@
 package com.evacipated.cardcrawl.mod.humilty
 
 import basemod.BaseMod
-import basemod.ModLabeledToggleButton
 import basemod.ModPanel
 import basemod.interfaces.EditStringsSubscriber
 import basemod.interfaces.PostInitializeSubscriber
 import com.evacipated.cardcrawl.mod.humilty.helpers.AssetLoader
-import com.evacipated.cardcrawl.modthespire.lib.SpireConfig
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer
-import com.megacrit.cardcrawl.core.CardCrawlGame
 import com.megacrit.cardcrawl.core.Settings
-import com.megacrit.cardcrawl.helpers.FontHelper
 import com.megacrit.cardcrawl.helpers.ImageMaster
 import com.megacrit.cardcrawl.localization.EventStrings
 import com.megacrit.cardcrawl.localization.MonsterStrings
@@ -28,8 +24,6 @@ class HumilityMod :
         val NAME: String
 
         val assets = AssetLoader()
-
-        private var config: SpireConfig? = null
 
         init {
             var tmpID = "humility"
@@ -50,49 +44,14 @@ class HumilityMod :
         @JvmStatic
         fun initialize() {
             BaseMod.subscribe(HumilityMod())
-
-            try {
-                val defaults = Properties().apply {
-                    this["DisableRealTime"] = false
-                }
-                config = SpireConfig(ID, "config", defaults)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
         }
 
         fun makeID(id: String) = "$ID:$id"
         fun assetPath(path: String) = "humilityAssets/$path"
-
-        fun disableRealTime(): Boolean {
-            return config?.getBool("DisableRealTime") ?: false
-        }
     }
 
     override fun receivePostInitialize() {
         val settingsPanel = ModPanel()
-
-        val disableRealTimeText = CardCrawlGame.languagePack.getUIString(makeID("OptionDisableRealTime"))?.TEXT ?: Array(2) { "[MISSING_TEXT]" }
-        ModLabeledToggleButton(
-            disableRealTimeText[0],
-            disableRealTimeText[1],
-            370f, 700f,
-            Settings.CREAM_COLOR,
-            FontHelper.charDescFont,
-            disableRealTime(),
-            settingsPanel,
-            {},
-            { toggle ->
-                config?.apply {
-                    setBool("DisableRealTime", toggle.enabled)
-                    try {
-                        save()
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    }
-                }
-            }
-        ).also { settingsPanel.addUIElement(it) }
 
         BaseMod.registerModBadge(
             ImageMaster.loadImage(assetPath("images/modBadge.png")),
